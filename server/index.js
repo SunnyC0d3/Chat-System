7fc953a1ef197db0958e145e383cf58667607f1d
Sukhwinder <singhdevelops@gmail.com>
2021-12-14 15:34:42 +0000

    Added cors to express

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ app.set('port', port);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', userRouter);
